4b8e4456081fa5371daeb724a6ef41e4fd735240
Tobias Lengsholz <tobias.lengsholz@invision.de>
2020-04-24 17:09:37 +0200

    Add first implementation of hook based solution
    Co-authored-by: Flo Becker <github@trashware.eu>

--- src/index.test.tsx
+++ src/index.test.tsx
@@ -1,160 +1,28 @@
 import * as React from 'react';
 import '@invisionag/jest-styled-components';
 import '@testing-library/jest-dom/extend-expect';
-import styled from 'styled-components';
-import { render } from '@testing-library/react';
-import styledown, { StyleDown as UnstyledStyleDown } from '.';
+import { render, screen, waitFor } from '@testing-library/react';
+import { useStyles } from '.';
 
 const TargetComponent: React.FC<React.HTMLAttributes<HTMLDivElement>> = (
   props,
-) => (
-  <div {...props} data-testid="target-component">
-    classname is {props.className}
-    {props.children}
-  </div>
-);
-
-const ComplicatedTargetComponent: React.FC<
-  React.HTMLAttributes<HTMLDivElement> & { passedClassName?: string }
-> = ({ passedClassName, ...props }) => (
-  <div {...props} data-testid="target-component">
-    <div className={passedClassName} data-testid="inner-target-component">
-      <span>classname is {props.className}</span>
-      <span>inner classname is {passedClassName}</span>
+) => {
+  const { className } = useStyles('background-color: red');
+  return (
+    <div {...props} data-testid="target-component" className={className}>
+      classname is {className}
+      {props.children}
     </div>
-  </div>
-);
+  );
+};
 
 describe('react-styledown', () => {
-  describe('styledown', () => {
-    it('passes on the styled-components class', () => {
-      const StyleDown = styledown`
-        background-color: red;
-      `;
+  it('attaches styles to the dom', async () => {
+    const { unmount, container, debug } = render(<TargetComponent />);
 
-      const { getByTestId } = render(<StyleDown>{TargetComponent}</StyleDown>);
+    const myComponent = screen.getByText(/classname is.*/);
 
-      expect(getByTestId('target-component')).toHaveStyleRule(
-        'background-color',
-        'red',
-      );
-    });
-
-    it('makes the classname usable inside the passed component', () => {
-      const StyleDown = styledown`
-        background-color: red;
-      `;
-
-      const { getByText } = render(<StyleDown>{TargetComponent}</StyleDown>);
-
-      expect(getByText(/classname.is..+/)).toBeInTheDocument();
-    });
-
-    it('allows for inline implementations to control classname', () => {
-      const StyleDown = styledown`
-        background-color: red;
-      `;
-
-      const { getByTestId } = render(
-        <StyleDown>
-          {({ className }) => (
-            <ComplicatedTargetComponent passedClassName={className} />
-          )}
-        </StyleDown>,
-      );
-
-      expect(getByTestId('target-component')).not.toHaveStyleRule(
-        'background-color',
-      );
-
-      expect(getByTestId('inner-target-component')).toHaveStyleRule(
-        'background-color',
-        'red',
-      );
-    });
-
-    it('passes props including children to the component passed to "as" prop', () => {
-      const StyleDown = styledown`
-        background-color: red;
-      `;
-
-      const { getByTestId, getByText } = render(
-        <StyleDown as={TargetComponent}>
-          <span>Hello World</span>
-        </StyleDown>,
-      );
-
-      expect(getByTestId('target-component')).toHaveStyleRule(
-        'background-color',
-        'red',
-      );
-      expect(getByText('Hello World')).toBeInTheDocument();
-    });
-  });
-
-  describe('StyleDown', () => {
-    it('works when not wrapping the StyleDown component in styled', () => {
-      const { getByText } = render(
-        <UnstyledStyleDown>{TargetComponent}</UnstyledStyleDown>,
-      );
-
-      expect(getByText('classname is')).toBeInTheDocument();
-    });
-
-    it('passes stuff through to child component', () => {
-      const { getByText } = render(
-        <UnstyledStyleDown className="custom class name">
-          {TargetComponent}
-        </UnstyledStyleDown>,
-      );
-
-      expect(getByText('classname is custom class name')).toBeInTheDocument();
-    });
-
-    it('passes on the styled-components class', () => {
-      const StyleDown = styled(UnstyledStyleDown)`
-        background-color: red;
-      `;
-
-      const { getByTestId } = render(<StyleDown>{TargetComponent}</StyleDown>);
-
-      expect(getByTestId('target-component')).toHaveStyleRule(
-        'background-color',
-        'red',
-      );
-    });
-
-    it('makes the classname usable inside the passed component', () => {
-      const StyleDown = styled(UnstyledStyleDown)`
-        background-color: red;
-      `;
-
-      const { getByText } = render(<StyleDown>{TargetComponent}</StyleDown>);
-
-      expect(getByText(/classname.is..+/)).toBeInTheDocument();
-    });
-
-    it('allows for inline implementations to control classname', () => {
-      const StyleDown = styled(UnstyledStyleDown)`
-        background-color: red;
-      `;
-
-      const { getByTestId } = render(
-        <StyleDown>
-          {({ className }) => (
-            <ComplicatedTargetComponent passedClassName={className} />
-          )}
-        </StyleDown>,
-      );
-
-      expect(getByTestId('target-component')).not.toHaveStyleRule(
-        'background-color',
-      );
-
-      expect(getByTestId('inner-target-component')).toHaveStyleRule(
-        'background-color',
-        'red',
-      );
-    });
+    expect(myComponent).toBeInTheDocument();
+    expect(myComponent).toHaveStyleRule('background-color: red');
   });
 });
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,49 @@
 import * as React from 'react';
+import ReactDOM from 'react-dom';
 import styled from 'styled-components';
 
-export type RenderFunctionProps = React.HTMLAttributes<HTMLDivElement>;
+function checksum(s: string) {
+  var hash = 0,
+    strlen = s.length,
+    i,
+    c;
+  if (strlen === 0) {
+    return hash.toString();
+  }
+  for (i = 0; i < strlen; i++) {
+    c = s.charCodeAt(i);
+    hash = (hash << 5) - hash + c;
+    hash = hash & hash; // Convert to 32bit integer
+  }
+  return hash.toString();
+}
 
-export type Props = RenderFunctionProps & {
-  children: React.ComponentType<RenderFunctionProps>;
-  as?: React.ComponentType<Partial<RenderFunctionProps>>;
+export const useStyles = (input: string) => {
+  const [className, setClassName] = React.useState<string>();
+  React.useEffect(() => {
+    // create styled component with passed styles
+    const StyleContainer = styled.div`
+      ${input}
+    `;
+
+    const inputChecksum = checksum(input);
+
+    // create (invisible) portal and render component there to attach styles
+    const orphanContainer = document.createElement('div');
+    ReactDOM.render(
+      <StyleContainer id={inputChecksum} />,
+      orphanContainer,
+      () => {
+        setClassName(
+          orphanContainer.querySelector(`#${inputChecksum}`)?.className,
+        );
+      },
+    );
+
+    return () => {
+      ReactDOM.unmountComponentAtNode(orphanContainer);
+    };
+  }, [input]);
+
+  return { className };
 };
-
-export const StyleDown: React.FC<Props> = ({
-  children: Component,
-  as: As,
-  ...props
-}) => (As ? <As {...props}>{Component}</As> : <Component {...props} />);
-
-export const styledown = styled(StyleDown);
-
-export default styledown;
