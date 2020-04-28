8bd3d0d181c16567f874bbcea8d5865a204bf416
Tobias Lengsholz <tobias.lengsholz@invision.de>
2020-04-28 09:50:43 +0200

    Remove internal identifier logic, it's not needed

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import ReactDOM from 'react-dom';
 import styled, { CSSObject, InterpolationFunction } from 'styled-components';
-import { generateAlphabeticName, hash } from './hash';
 
 export function useStyles<T = {}>(
   props?: React.HTMLAttributes<HTMLDivElement> & T,
@@ -19,26 +18,16 @@ export function useStyles<T = {}>(
       // create styled component with passed styles
       const StyleContainer = styled.div(literal, expressions);
 
-      const internalClassName = generateAlphabeticName(
-        hash(literal.toString()),
-      );
-
       // create (invisible) portal and render component there to attach styles
       const orphanContainer = document.createElement('div');
-      ReactDOM.render(
-        <StyleContainer {...props} className={internalClassName} />,
-        orphanContainer,
-        () => {
-          const generatedClassName = orphanContainer.querySelector(
-            `.${internalClassName}`,
-          )?.className;
-          setClassName(
-            props?.className
-              ? `${props?.className} ${generatedClassName}`
-              : generatedClassName,
-          );
-        },
-      );
+      ReactDOM.render(<StyleContainer {...props} />, orphanContainer, () => {
+        const generatedClassName = orphanContainer.children[0]?.className;
+        setClassName(
+          props?.className
+            ? `${props?.className} ${generatedClassName}`
+            : generatedClassName,
+        );
+      });
 
       return () => {
         ReactDOM.unmountComponentAtNode(orphanContainer);
